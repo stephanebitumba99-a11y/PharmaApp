8ac5009f0a96fcef7a7610f1582d513245308aa5
Stephane BITUMBA <stephanebitumba99@gmail.com>
2026-06-26 09:33:46 +0200

    Ajout des dependances

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -7,6 +8,12 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+
+    // ✅ IMPORTANT pour Laravel API
+    provideHttpClient(withFetch()),
+
+    // hydration Angular SSR (ok tu peux laisser)
+    provideClientHydration(withEventReplay())
   ]
 };
